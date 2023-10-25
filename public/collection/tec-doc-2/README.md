75bcc06844c7cf34ed9da8354109443f4c1fe992
Joe Rawnsley <joe.rawnsley@gmail.com>
2023-10-25 17:17:51 +0100

    updated blurb on landing page and readme in tec-doc-2

--- public/collection/tec-doc-2/README.md
+++ public/collection/tec-doc-2/README.md
@@ -15,3 +15,4 @@ Full Collection at https://joes-mini-collection.netlify.app/
 
 - HTML
 - CSS
+- Bootstrap
--- src/blurb.js
+++ src/blurb.js
@@ -4,10 +4,8 @@ const Blurb = () => {
   return (
     <div className="main-element">
         <p className="blurb">
-        This Mini-collection is not intended to be a portfolio, or to demonstrate a professional standard of work. The landing page and the items it links to are not finished products... yet. Instead, this collection is intended to document my learning journey and act as a focal point and a place for me to experiment, which may eventually evolve into something more polished. Please feel free to get in touch if you have any questions - joe.rawnsley@gmail.com
-        </p>
-        
-        
+        I'm Joseph Rawnsley, a budding software developer based in Manchester, UK. This Mini-collection is intended to document my learning journey and act as a focal point and a place for me to experiment, which may eventually evolve into a portfolio of work. The projects make use of JavaScript, React, Bootstrap, HTML, CSS and Netlify Serverless functions. You can contact me at joe.rawnsley@gmail.com
+        </p>        
     </div>
   )
 }
