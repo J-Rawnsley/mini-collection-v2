58ea557c7f21a7833c74c078e639f45b3ba83388
Joe Rawnsley <joe.rawnsley@gmail.com>
2023-10-29 10:39:55 +0000

    updated blurb

--- src/blurb.js
+++ src/blurb.js
@@ -4,7 +4,7 @@ const Blurb = () => {
   return (
     <div className="main-element">
         <p className="blurb">
-        I'm Joseph Rawnsley, a budding software developer based in Manchester, UK. This Mini-collection is intended to document my learning journey and act as a focal point and a place for me to experiment, which may eventually evolve into a portfolio of work. The projects make use of JavaScript, React, Bootstrap, HTML, CSS and Netlify Serverless functions. You can contact me at joe.rawnsley@gmail.com
+        I'm Joe Rawnsley, a budding software developer based in Manchester, UK. This collection of work is intended to document my learning journey and act as a focal point, a place for me to test and share my work, which may eventually evolve into a portfolio. The projects make use of JavaScript, React, Bootstrap, HTML, CSS and Netlify Serverless functions. You can contact me at joe.rawnsley@gmail.com
         </p>        
     </div>
   )
